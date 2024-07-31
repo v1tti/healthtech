685a5eec9c5b2a5929c0aa39494e595f00f585d5
v1tti <bielvitti@hotmail.com>
2024-07-31 19:48:19 -0300

    adding better qr reading

--- src/qr-reader/qrReader.jsx
+++ src/qr-reader/qrReader.jsx
@@ -1,53 +1,36 @@
 import { useState, useEffect } from "react";
-import { Link, useNavigate } from "react-router-dom";
 import { Header } from "../ui/components/header/header";
 import "./qrReader.css";
 import { Html5QrcodeScanner } from "html5-qrcode";
+import { Link, useNavigate } from "react-router-dom";
 import { ROUTES } from "../router/routes-constants";
+import Html5QrcodePlugin from "./Html5QrcodePlugin";
 
 export const QRCodeReader = () => {
-  const [scanner, setScanner] = useState(null);
-  const [cameraFacingMode, setCameraFacingMode] = useState("user"); // 'user' for front camera, 'environment' for rear camera
-
-
-  useEffect(() => {
-    const newScanner = new Html5QrcodeScanner("reader", {
-      qrbox: { width: 250, height: 250 },
-      fps: 5,
-      facingMode: cameraFacingMode // Configuração da câmera
-    });
-
-    newScanner.render(onScanSuccess, onScanError);
-
-    setScanner(newScanner);
-
-    return () => {
-      newScanner.clear();
+  const [decodedResults, setDecodedResults] = useState([]);
+  const navigate = useNavigate()
+    const onNewScanResult = (decodedText, decodedResult) => {
+        console.log("App [result]", decodedResult);
+        setDecodedResults(prev => [...prev, decodedResult]);
+        const url = decodedResult.decodedText;
+const path = url.split("/").pop(); 
+const upperCasePath = path.toUpperCase(); 
+console.log(upperCasePath);
+        navigate(`${ROUTES[upperCasePath]}`)
     };
-  }, [cameraFacingMode]);
 
-  const onScanSuccess = (result) => {
-    scanner.clear();
-    return <Link to={ROUTES.SQUAT}></Link> 
-  };
+    return (
+        <div className="App">
+            <section className="App-section">
+                <div className="App-section-title"> Html5-qrcode React demo</div>
+                <Html5QrcodePlugin
+                    fps={10}
+                    qrbox={250}
+                    disableFlip={false}
+                    qrCodeSuccessCallback={onNewScanResult}
+                />
 
-  const onScanError = (err) => {
-    console.warn(err);
-  };
-
-  const toggleCamera = () => {
-    setCameraFacingMode((prevMode) =>
-      prevMode === "user" ? "environment" : "user"
+            </section>
+        </div>
     );
-  };
-
-  return (
-    <div className="container">
-      <Header />
-      <button onClick={toggleCamera}>
-        {cameraFacingMode === "user" ? "Switch to Rear Camera" : "Switch to Front Camera"}
-      </button>
-      <div id="reader"></div>
-    </div>
-  );
 };
